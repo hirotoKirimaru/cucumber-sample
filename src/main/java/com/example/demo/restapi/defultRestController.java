8a6c92822b9b38b9c7d7f560eef070f6b6ca8d28
nainaistar <nainaistar@gmail.com>
2019-06-08 22:47:22 +0900

    feat:RestAPIのテスト追加

--- src/main/java/com/example/demo/restapi/defultRestController.java
+++ src/main/java/com/example/demo/restapi/defultRestController.java
@@ -1,14 +1,26 @@
 package com.example.demo.restapi;
 
-import org.springframework.web.bind.annotation.Mapping;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
 
 @RestController
 public class defultRestController {
 
     @RequestMapping("/")
-    public String hogeApi() {
+    public String hogeApi(@Validated Param param) {
+        System.out.println(param.getPiyo());
         return "hogehoge";
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class Param {
+        @NotNull
+        private String piyo;
     }
 }
--- src/test/java/com/example/demo/restapi/defultRestControllerTest.java
+++ src/test/java/com/example/demo/restapi/defultRestControllerTest.java
@@ -1,14 +1,43 @@
 package com.example.demo.restapi;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(defultRestController.class)
 class defultRestControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    public static void hoge() {
+    void success() throws Exception {
+        this.mockMvc.perform(get("/").param("piyo", "aiueo"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hogehoge"));
+    }
+
+
+    @Test
+    void hogeError() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(""));
+
 
     }
 
+    @Nested
+    public class paramValidate {
+        @Autowired
+        private MockMvc mockMvc;
+
+
+    }
 }
