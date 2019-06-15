6d247b5d3e36fcc4a8ec708a59c638c2f3c60bea
nainaistar <nainaistar@gmail.com>
2019-06-15 23:56:32 +0900

    feat:ゴミファイル削除

--- src/test/java/RunCucumber.java
+++
@@ -1,9 +0,0 @@
-//import cucumber.api.CucumberOptions;
-//import cucumber.api.junit.Cucumber;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.junit.runner.RunWith;
-//
-//@RunWith(Cucumber.class)
-//@CucumberOptions
-//public class RunCucumber {
-//}
