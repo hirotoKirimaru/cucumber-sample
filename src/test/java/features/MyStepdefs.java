03a2a94a8831edbda91750aec47554043e9d7751
nainaistar <nainaistar@gmail.com>
2019-06-15 10:12:41 +0900

    feat:selenide実装

--- src/test/java/features/MyStepdefs.java
+++ src/test/java/features/MyStepdefs.java
@@ -1,17 +1,25 @@
 package features;
 
+import com.codeborne.selenide.Selenide;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 
 public class MyStepdefs {
-    @Given("aiueo")
-    public void aiueo() {
+
+
+    @Given("topページに遷移する")
+    public void topページに遷移する() {
+        Selenide.open("");
     }
 
-    @Given("ague")
-    public void ague() {
+    @When("ログインボタンを押す")
+    public void ログインボタンを押す() {
+        Selenide.$("#login").click();
+    }
+
+    @When("ログアウトボタンを押す")
+    public void ログアウトボタンを押す() {
         // Write code here that turns the phrase above into concrete actions
         throw new cucumber.api.PendingException();
     }
-
-
 }
