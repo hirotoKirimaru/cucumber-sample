61589690a43ffbade848b69e3c47230ec52ba388
nainaistar <nainaistar@gmail.com>
2020-05-24 15:10:45 +0900

    [Add]　Jsonをクラスに変換することでいろいろと楽にするやつ

---
+++ src/main/java/jp/co/kelly/biz/domain/Human.java
@@ -0,0 +1,15 @@
+package jp.co.kelly.biz.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class Human {
+    private String name;
+    private int age;
+}
