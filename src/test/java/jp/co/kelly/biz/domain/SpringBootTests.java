5d824288a295921560def962ab900ac4b020289e
nainaistar <nainaistar@gmail.com>
2020-08-08 15:49:40 +0900

    [Add] SpringBootTest

--- src/test/java/jp/co/kelly/biz/domain/SpringBootTests.java
+++ src/test/java/jp/co/kelly/biz/domain/SpringBootTests.java
@@ -4,8 +4,6 @@ package jp.co.kelly.biz.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.time.LocalDateTime;
 
@@ -13,16 +11,12 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
  * SpringJunitConfigを使うと、SpringBootTestと異なって、必要なComponentのみをScanできるようになるので、テスト時間が短くなる。
- * ただ、特定のクラスだけをAutowiredしたい場合は、{@link SpringBootTests}のやり方がいいかも。
+ * ただ、特定のクラスだけをAutowiredしたい場合は、
  */
-@SpringJUnitConfig(SpringJunitConfigTest.Config.class)
-class SpringJunitConfigTest {
+@SpringBootTest(classes = DateConverter.class)
+class SpringBootTests {
     @Autowired
     DateConverter target;
-
-    @ComponentScan({"jp.co.kelly.biz.domain"})
-    static class Config {
-    }
 
     @Test
     void test_01() {
