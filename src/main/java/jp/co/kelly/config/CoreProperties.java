4074a300bf52eabcb60596fdf1920a0a20091d42
nainaistar <nainaistar@gmail.com>
2020-10-11 22:10:14 +0900

    [Add] Componentを削除

--- src/main/java/jp/co/kelly/config/CoreProperties.java
+++ src/main/java/jp/co/kelly/config/CoreProperties.java
@@ -3,12 +3,10 @@ package jp.co.kelly.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "app.config")
-@Component
 public class CoreProperties {
   private String appName;
 }
