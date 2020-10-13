ddee8dafe4ed655d81016341a513019eda3fe35e
nainaistar <nainaistar@gmail.com>
2020-10-13 17:07:02 +0900

    [Add] 上書きも可能

---
+++ src/test/java/jp/co/kelly/config/FacebookPropertiesTests.java
@@ -0,0 +1,24 @@
+package jp.co.kelly.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringJUnitConfig(initializers = ConfigFileApplicationContextInitializer.class)
+@EnableConfigurationProperties({FacebookProperties.class})
+class FacebookPropertiesTests {
+  @Autowired
+  FacebookProperties properties;
+
+  @Test
+  void test_01() {
+    URI url = properties.getUri();
+    assertThat(url.toString()).isEqualTo("http://localhost:10080/facebook");
+  }
+}
