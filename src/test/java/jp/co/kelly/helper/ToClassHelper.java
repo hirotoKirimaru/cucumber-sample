9fe3220c5f54ae3c68bcd05a2ba5f37080acf60b
nainaistar <nainaistar@gmail.com>
2020-10-18 13:50:51 +0900

    [Add] Linuxでも動くようにする

--- src/test/java/jp/co/kelly/helper/ToClassHelper.java
+++ src/test/java/jp/co/kelly/helper/ToClassHelper.java
@@ -20,61 +20,61 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
 public class ToClassHelper {
-    ObjectMapper objectMapper = new ObjectMapper();
+  ObjectMapper objectMapper = new ObjectMapper();
 
-    <T> T fromJson(Path path, Class<T> clazz) {
-        File file = path.toFile();
-        try (FileInputStream input = new FileInputStream(file)) {
+  <T> T fromJson(Path path, Class<T> clazz) {
+    File file = path.toFile();
+    try (FileInputStream input = new FileInputStream(file)) {
 
-            String json = IOUtils.toString(input, StandardCharsets.UTF_8);
-            return fromJson(json, clazz);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException("何か失敗した", e);
-        }
+      String json = IOUtils.toString(input, StandardCharsets.UTF_8);
+      return fromJson(json, clazz);
+    } catch (Exception e) {
+      e.printStackTrace();
+      throw new RuntimeException("何か失敗した", e);
     }
+  }
 
-    <T> T fromJson(String json, Class<T> clazz) {
-        try {
-            return objectMapper.readValue(json, clazz);
-        } catch (IOException e) {
-            throw new RuntimeException("Json変換できませんでした", e);
-        }
+  <T> T fromJson(String json, Class<T> clazz) {
+    try {
+      return objectMapper.readValue(json, clazz);
+    } catch (IOException e) {
+      throw new RuntimeException("Json変換できませんでした", e);
     }
+  }
 
 
-    @Test
-    public void test_01() {
-        //language=JSON
-        String json = "{\n" +
-                "  \"age\": 18,\n" +
-                "  \"name\": \"kirimaru\"\n" +
-                "}";
+  @Test
+  public void test_01() {
+    //language=JSON
+    String json = "{\n" +
+        "  \"age\": 18,\n" +
+        "  \"name\": \"kirimaru\"\n" +
+        "}";
 
 
-        Human expect = Human.builder()
-                .age(18)
-                .name("kirimaru")
-                .build();
+    Human expect = Human.builder()
+        .age(18)
+        .name("kirimaru")
+        .build();
 
-        Human actual = fromJson(json, Human.class);
+    Human actual = fromJson(json, Human.class);
 
-        assertThat(actual).isEqualTo(expect);
-    }
+    assertThat(actual).isEqualTo(expect);
+  }
 
-    @Test
-    public void test_02() {
-        Path path = Paths.get("src\\test\\resources", "jp\\co\\kelly\\human.json");
+  @Test
+  public void test_02() {
+    Path path = Paths.get("src", "test", "resources", "jp", "co", "kelly", "human.json");
 
-        Human expect = Human.builder()
-                .age(18)
-                .name("kirimaru")
-                .build();
+    Human expect = Human.builder()
+        .age(18)
+        .name("kirimaru")
+        .build();
 
-        Human actual = fromJson(path, Human.class);
+    Human actual = fromJson(path, Human.class);
 
-        assertThat(actual).isEqualTo(expect);
-    }
+    assertThat(actual).isEqualTo(expect);
+  }
 
 
 }
