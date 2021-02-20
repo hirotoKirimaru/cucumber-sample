805400ba6a601fc5312fe15956f335aa2fd9c2c8
nainaistar <nainaistar@gmail.com>
2021-02-20 13:37:15 +0900

    [Add] IOExceptionが発生する…

--- src/main/java/jp/co/kelly/biz/domain/weather/WeatherDomainService.java
+++ src/main/java/jp/co/kelly/biz/domain/weather/WeatherDomainService.java
@@ -1,15 +1,43 @@
 package jp.co.kelly.biz.domain.weather;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.filter.TokenFilter;
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectReader;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import lombok.RequiredArgsConstructor;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.List;
 
 @RequiredArgsConstructor
 public class WeatherDomainService {
-  private final ObjectMapper objectMapper;
+  private final CsvMapper csvMapper;
+
   public List<Weather> readCsc() {
+//    CsvSchema csvSchema = csvMapper.schemaFor(Weather.class).withHeader();
+//
+//
+//    InputStream resourceAsStream = getClass().getResourceAsStream("/jp/co/kelly/2014ebina.csv");
+////      List<Weather> list =
+//    MappingIterator<Weather> objectMappingIterator = null;
+//    try {
+//      objectMappingIterator = csvMapper.readerFor(Weather.class).with(csvSchema).readValues(resourceAsStream);
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    }
+////      MappingIterator<Weather> objectMappingIterator =
+//
+//      while(objectMappingIterator.hasNext()){
+//        Object next = objectMappingIterator.next();
+//      }
+////      return csvMapper.schema(csvSchema).readValue(resourceAsStream, List.class);
+//
+////    } catch (Exception e) {
+////      e.printStackTrace();
+////    }
 
     return List.of();
   }
--- src/test/java/jp/co/kelly/biz/domain/weather/WeatherDomainServiceTests.java
+++ src/test/java/jp/co/kelly/biz/domain/weather/WeatherDomainServiceTests.java
@@ -1,6 +1,7 @@
 package jp.co.kelly.biz.domain.weather;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -9,11 +10,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class WeatherDomainServiceTests {
-  WeatherDomainService target = new WeatherDomainService(new ObjectMapper());
+  WeatherDomainService target = new WeatherDomainService(new CsvMapper());
   @Test
   void test_01() {
     var weathers = target.readCsc();
-
     assertThat(weathers).isEmpty();
   }
 }
