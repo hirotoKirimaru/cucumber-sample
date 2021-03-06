2a05dae149d15bedec006bc6b929ac1f42591abb
nainaistar <nainaistar@gmail.com>
2021-03-06 17:48:49 +0900

    [Add] ログの確認をする

--- src/main/java/jp/co/kelly/restapi/interceptor/MaintenanceInterceptor.java
+++ src/main/java/jp/co/kelly/restapi/interceptor/MaintenanceInterceptor.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.WebRequestInterceptor;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.springframework.web.servlet.handler.WebRequestHandlerInterceptorAdapter;
 
@@ -14,33 +15,45 @@ import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
+import java.util.Arrays;
 
 @Slf4j
 //@Component
 //public class MaintenanceInterceptor extends WebRequestHandlerInterceptorAdapter {
-  public class MaintenanceInterceptor implements AsyncHandlerInterceptor {
-//  public class MaintenanceInterceptor extends HandlerInterceptorAdapter {
-//  /**
-//   * Create a new WebRequestHandlerInterceptorAdapter for the given WebRequestInterceptor.
-//   *
-//   * @param requestInterceptor the WebRequestInterceptor to wrap
-//   */
-//  public MaintenanceInterceptor(WebRequestInterceptor requestInterceptor) {
-//    super(requestInterceptor);
-//  }
+public class MaintenanceInterceptor implements AsyncHandlerInterceptor {
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
 
 //    super.preHandle(request, response, handler);
-    LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Tokyo"));
-
-    if (now.isBefore(LocalDateTime.MAX)) {
-      log.error("メンテナンス中です");
-      return false;
-    }
+//    LocalDateTime now = LocalDateTime.now(ZoneId.of("Asia/Tokyo"));
+//
+//    if (now.isBefore(LocalDateTime.MAX)) {
+//      log.error("メンテナンス中です");
+//      return false;
+//    }
+    log.info("preHandle");
+//    throw new RuntimeException("Error");
 
     return true;
   }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                         @Nullable ModelAndView modelAndView) throws Exception {
+
+    log.error("postHandle");
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                              @Nullable Exception ex) throws Exception {
+    log.error("afterCompletion");
+    if (ex != null) {
+      log.error(Arrays.toString(ex.getStackTrace()));
+    }
+  }
+
+
 }
