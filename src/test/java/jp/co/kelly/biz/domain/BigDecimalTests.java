c398a8a8028ed9759c33bde91abfb45931a499cd
nainaistar <nainaistar@gmail.com>
2021-03-12 01:15:26 +0900

    [Add] BigDecimalの挙動確認

---
+++ src/test/java/jp/co/kelly/biz/domain/BigDecimalTests.java
@@ -0,0 +1,27 @@
+package jp.co.kelly.biz.domain;
+
+import cucumber.api.java.it.Ma;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BigDecimalTests {
+  @Test
+  void test_01() {
+
+    assertThat(
+        BigDecimal.valueOf(2)
+            .setScale(10, RoundingMode.HALF_UP)
+            .divide(BigDecimal.valueOf(3), RoundingMode.HALF_UP)
+            .multiply(BigDecimal.valueOf(100))
+            .setScale(2, RoundingMode.HALF_UP)
+    ).isEqualTo(BigDecimal.valueOf(66.67));
+
+
+  }
+}
