00dff5bd3d37c8829ad7bae3348b93c71a45f6f3
nainaistar <nainaistar@gmail.com>
2021-03-20 11:58:55 +0900

    BigDecimalの処理

--- src/test/java/jp/co/kelly/biz/domain/BigDecimalTests.java
+++ src/test/java/jp/co/kelly/biz/domain/BigDecimalTests.java
@@ -1,5 +1,6 @@
 package jp.co.kelly.biz.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -13,12 +14,27 @@ class BigDecimalTests {
 
     assertThat(
         BigDecimal.valueOf(2)
-            .setScale(10, RoundingMode.HALF_UP)
-            .divide(BigDecimal.valueOf(3), RoundingMode.HALF_UP)
+            .divide(BigDecimal.valueOf(3), 10, RoundingMode.HALF_UP)
             .multiply(BigDecimal.valueOf(100))
             .setScale(2, RoundingMode.HALF_UP)
     ).isEqualTo(BigDecimal.valueOf(66.67));
+  }
 
+  @DisplayName("0と0.00では=にならない。")
+  @Test
+  void test_02() {
+    assertThat(
+        BigDecimal.ZERO.equals(BigDecimal.ZERO.setScale(2))
+    ).isEqualTo(false);
+  }
 
+  @Test
+  void test_03() {
+    BigDecimal one = BigDecimal.ZERO;
+    BigDecimal two = BigDecimal.ZERO.setScale(2);
+
+    assertThat(
+        one.signum() == two.signum()
+    ).isEqualTo(true);
   }
 }
