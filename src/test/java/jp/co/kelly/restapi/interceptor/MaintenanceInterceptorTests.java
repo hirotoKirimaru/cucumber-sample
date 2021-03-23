59bd4ae1b85b9db31647b5b723ac3cc754cb9816
nainaistar <nainaistar@gmail.com>
2021-03-23 22:15:55 +0900

    unused remove

--- src/test/java/jp/co/kelly/restapi/interceptor/MaintenanceInterceptorTests.java
+++ src/test/java/jp/co/kelly/restapi/interceptor/MaintenanceInterceptorTests.java
@@ -1,18 +1,12 @@
 package jp.co.kelly.restapi.interceptor;
 
-import io.restassured.module.mockmvc.internal.StandaloneMockMvcFactory;
 import jp.co.kelly.restapi.AnimalsRestController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
-import org.springframework.web.context.request.WebRequestInterceptor;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
