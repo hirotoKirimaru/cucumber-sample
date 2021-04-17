3ec26e09c62f28e7645845f8a9f3e5c8b3aeed84
nainaistar <nainaistar@gmail.com>
2021-04-17 21:58:34 +0900

    [Add] MessageFormatで持たせる

--- src/main/java/kirimaru/biz/domain/constant/ErrorCode.java
+++ src/main/java/kirimaru/biz/domain/constant/ErrorCode.java
@@ -1,27 +1,29 @@
 package kirimaru.biz.domain.constant;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 
 import java.text.MessageFormat;
-import java.util.Arrays;
 import java.util.Objects;
 
-@AllArgsConstructor
 @Getter
 public enum ErrorCode {
   E0001("E00001", "致命的なエラーが発生しました。"),
   E0002("E00002", "{0}は必須です。", 1),
   E0003("E00003", "{0}が{1}の時、{2}は必須です。", 3);
   private final String id;
-  private final String message;
+  private final MessageFormat message;
   private final int prepareNum;
 
   ErrorCode(String id, String message) {
     this.id = id;
-    this.message = message;
+    this.message = new MessageFormat(message);
     this.prepareNum = 0;
+  }
+  ErrorCode(String id, String message, int prepareNum) {
+    this.id = id;
+    this.message = new MessageFormat(message);
+    this.prepareNum = prepareNum;
   }
 
   public String fillMessages(@NonNull String... values) {
@@ -32,7 +34,7 @@ public enum ErrorCode {
     if (values.length != prepareNum) {
       throw new RuntimeException("コンパイルエラーにしたい！");
     }
-    return MessageFormat.format(message, values);
+    return message.format(values);
   }
 
 }
