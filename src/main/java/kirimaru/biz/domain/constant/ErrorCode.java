8c4ee5740b5afdccdae38c663a1b56018059f8a1
nainaistar <nainaistar@gmail.com>
2021-04-17 21:54:37 +0900

    [Add] null回避をする

--- src/main/java/kirimaru/biz/domain/constant/ErrorCode.java
+++ src/main/java/kirimaru/biz/domain/constant/ErrorCode.java
@@ -2,17 +2,18 @@ package kirimaru.biz.domain.constant;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NonNull;
 
 import java.text.MessageFormat;
+import java.util.Arrays;
+import java.util.Objects;
 
 @AllArgsConstructor
 @Getter
 public enum ErrorCode {
   E0001("E00001", "致命的なエラーが発生しました。"),
   E0002("E00002", "{0}は必須です。", 1),
-  E0003("E00003", "{0}が{1}の時、{2}は必須です。", 3)
-
-  ;
+  E0003("E00003", "{0}が{1}の時、{2}は必須です。", 3);
   private final String id;
   private final String message;
   private final int prepareNum;
@@ -23,7 +24,14 @@ public enum ErrorCode {
     this.prepareNum = 0;
   }
 
-  public String fillMessages(String... values){
+  public String fillMessages(@NonNull String... values) {
+    for (String value : values) {
+      Objects.requireNonNull(value);
+    }
+
+    if (values.length != prepareNum) {
+      throw new RuntimeException("コンパイルエラーにしたい！");
+    }
     return MessageFormat.format(message, values);
   }
 
--- src/test/java/kirimaru/biz/domain/constant/ErrorCodeTests.java
+++ src/test/java/kirimaru/biz/domain/constant/ErrorCodeTests.java
@@ -3,7 +3,10 @@ package kirimaru.biz.domain.constant;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import javax.validation.constraints.Null;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ErrorCodeTests {
@@ -21,6 +24,25 @@ class ErrorCodeTests {
       assertThat(
           ErrorCode.E0002.fillMessages("ゴリラ")
       ).isEqualTo("ゴリラは必須です。");
+    }
+
+    @Test
+    void test_03() {
+      assertThatThrownBy(
+          () -> ErrorCode.E0002.fillMessages("ゴリラ", "キリン")
+      ).isInstanceOf(RuntimeException.class);
+    }
+    @Test
+    void test_04() {
+      assertThatThrownBy(
+          () -> ErrorCode.E0003.fillMessages("ゴリラ", null, null)
+      ).isInstanceOf(NullPointerException.class);
+    }
+    @Test
+    void test_05() {
+      assertThatThrownBy(
+          () -> ErrorCode.E0003.fillMessages(null, null, null)
+      ).isInstanceOf(NullPointerException.class);
     }
   }
 
