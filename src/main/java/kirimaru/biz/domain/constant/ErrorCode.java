2a895d7a7ed8b811bcd470c170c3e5424d611756
nainaistar <nainaistar@gmail.com>
2021-04-26 00:14:38 +0900

    [Add] 注意書き追加

--- src/main/java/kirimaru/biz/domain/constant/ErrorCode.java
+++ src/main/java/kirimaru/biz/domain/constant/ErrorCode.java
@@ -12,23 +12,28 @@ public enum ErrorCode {
   E0002("E00002", "{0}は必須です。", 1),
   E0003("E00003", "{0}が{1}の時、{2}は必須です。", 3);
   private final String id;
-  private final MessageFormat message;
+  private final String message;
   private final int prepareNum;
 
   ErrorCode(String id, String message) {
     this.id = id;
-    this.message = new MessageFormat(message);
+    this.message = message;
     this.prepareNum = 0;
   }
+
   ErrorCode(String id, String message, int prepareNum) {
     this.id = id;
-    this.message = new MessageFormat(message);
+    this.message = message;
     this.prepareNum = prepareNum;
   }
 
+  /**
+   * ※ MessageFormatはスレッドセーフではないので、毎回生成すること。
+   * 遅いことは遅いが、1マイクロ秒以下で生成されるので速度に大きな影響はない
+   */
   public String fillMessages(@NonNull String... values) {
     validate(values);
-    return message.format(values);
+    return MessageFormat.format(message, values);
   }
 
 
