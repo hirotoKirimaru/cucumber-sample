d77710a35b451c2179bff08bd012f5e4895894e4
nainaistar <nainaistar@gmail.com>
2021-04-28 03:08:47 +0900

    [Add] while

--- src/main/java/kirimaru/biz/domain/NumToNumArray.java
+++ src/main/java/kirimaru/biz/domain/NumToNumArray.java
@@ -25,12 +25,9 @@ public class NumToNumArray {
 
     List<Integer> list = new ArrayList<>();
 //    List<Integer> list = new ArrayList<>(String.valueOf(i).length()); // 初期配列用意してる時間の方が長い
-    while (true) {
+    while (i != 0) {
       list.add(i % 10);
       i /= 10;
-      if (i == 0) {
-        break;
-      }
     }
 
     return list;
