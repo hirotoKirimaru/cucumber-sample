511e48a8f9c90ce2752f8526b776c032fbbc29fd
nainaistar <nainaistar@gmail.com>
2021-04-30 12:28:02 +0900

    [Add] とりあえずコピペ

--- src/test/java/kirimaru/biz/domain/ContractTest.java
+++ src/test/java/kirimaru/biz/domain/ContractTest.java
@@ -24,6 +24,43 @@ class ContractTest {
           .expireDate(end)
           .build();
 
+      assertThat(target.canExpire()).isEqualTo(result);
+    }
+
+    private Stream<Arguments> canExpire() {
+      return Stream.of(
+          Arguments.of(LocalDate.of(2020, 12, 10), LocalDate.of(2021, 2, 9), true),
+          Arguments.of(LocalDate.of(2020, 12, 10), LocalDate.of(2021, 2, 1), false),
+          Arguments.of(LocalDate.of(2020, 12, 1), LocalDate.of(2021, 3, 31), true),
+          Arguments.of(LocalDate.of(2020, 12, 1), LocalDate.of(2021, 3, 30), false),
+          Arguments.of(LocalDate.of(2020, 12, 1), LocalDate.of(2021, 2, 28), true),
+          Arguments.of(LocalDate.of(2020, 12, 1), LocalDate.of(2024, 2, 29), true),
+          Arguments.of(LocalDate.of(2020, 12, 31), LocalDate.of(2021, 3, 30), true),
+          Arguments.of(LocalDate.of(2020, 12, 31), LocalDate.of(2021, 4, 30), true),
+          Arguments.of(LocalDate.of(2020, 12, 31), LocalDate.of(2021, 3, 31), false),
+          Arguments.of(LocalDate.of(2020, 11, 30), LocalDate.of(2021, 3, 31), false),
+          Arguments.of(LocalDate.of(2020, 12, 31), LocalDate.of(2021, 2, 28), true),
+          Arguments.of(LocalDate.of(2020, 12, 31), LocalDate.of(2024, 2, 29), true),
+          Arguments.of(LocalDate.of(2020, 2, 29), LocalDate.of(2024, 12, 31), false),
+          Arguments.of(LocalDate.of(2020, 2, 29), LocalDate.of(2024, 11, 30), false)
+
+
+      );
+    }
+  }
+
+
+  @Nested
+  @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+  class CanExpire2 {
+    @MethodSource(value = "canExpire")
+    @ParameterizedTest(name = "契約日が{0}, 解約日が{1}の時、{2}")
+    void test_01(LocalDate start, LocalDate end, boolean result) {
+      var target = Contract.builder()
+          .contractDate(start)
+          .expireDate(end)
+          .build();
+
       assertThat(target.canExpire2()).isEqualTo(result);
     }
 
