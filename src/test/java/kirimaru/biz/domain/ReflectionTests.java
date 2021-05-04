a7f2f1c6e4675e10a33a2f5f1890e0d87f41a9d6
nainaistar <nainaistar@gmail.com>
2021-05-04 22:53:14 +0900

    [Add] Disabled

--- src/test/java/kirimaru/biz/domain/ReflectionTests.java
+++ src/test/java/kirimaru/biz/domain/ReflectionTests.java
@@ -4,6 +4,7 @@ package kirimaru.biz.domain;
 import kirimaru.biz.domain.nest.Child;
 import kirimaru.biz.domain.nest.GrandChild;
 import kirimaru.biz.domain.nest.Parent;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -49,6 +50,29 @@ class ReflectionTests {
 //      ).isEqualTo("説明");
 
 
+    }
+
+    @Disabled("あとで再度調査する")
+    @DisplayName("privateなものを取得する")
+    @Test
+    void test_02() throws Exception {
+      Parent target = Parent.builder()
+          .child(Child.builder()
+              .grandChild(GrandChild.builder()
+                  .description("説明")
+                  .build())
+              .build())
+          .build();
+
+      SpelParserConfiguration config = new SpelParserConfiguration(true, true);
+      StandardEvaluationContext context = new StandardEvaluationContext(target);
+
+      ExpressionParser expressionParser = new SpelExpressionParser(config);
+      Object description = expressionParser.parseExpression("child.grandChild.description").getValue(context);
+
+      assertThat(
+          description
+      ).isEqualTo("説明");
     }
   }
 
