d1ffc0f0d69d7b679304301418d71c367cc49c66
nainaistar <nainaistar@gmail.com>
2021-05-10 22:22:19 +0900

    [Add] 月末を求める

--- src/test/java/kirimaru/biz/domain/YearMonthTests.java
+++ src/test/java/kirimaru/biz/domain/YearMonthTests.java
@@ -14,4 +14,25 @@ public class YearMonthTests {
         YearMonth.parse("2020-01").atEndOfMonth()
     ).isEqualTo(LocalDate.of(2020, 1, 31));
   }
+
+  @Test
+  void test_02() {
+    assertThat(
+        YearMonth.parse("2020-04").atEndOfMonth()
+    ).isEqualTo(LocalDate.of(2020, 4, 30));
+  }
+
+  @Test
+  void test_03() {
+    assertThat(
+        YearMonth.parse("2020-02").atEndOfMonth()
+    ).isEqualTo(LocalDate.of(2020, 2, 29));
+  }
+
+  @Test
+  void test_04() {
+    assertThat(
+        YearMonth.parse("2021-02").atEndOfMonth()
+    ).isEqualTo(LocalDate.of(2021, 2, 28));
+  }
 }
