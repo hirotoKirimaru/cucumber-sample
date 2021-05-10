51926446fa13c430d60a940aaed1991cc59004ea
nainaistar <nainaistar@gmail.com>
2021-05-10 22:29:15 +0900

    [Add] LocalDateから月末を求める

--- src/test/java/kirimaru/biz/domain/YearMonthTests.java
+++ src/test/java/kirimaru/biz/domain/YearMonthTests.java
@@ -1,5 +1,6 @@
 package kirimaru.biz.domain;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -8,31 +9,79 @@ import java.time.YearMonth;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class YearMonthTests {
-  @Test
-  void test_01() {
-    assertThat(
-        YearMonth.parse("2020-01").atEndOfMonth()
-    ).isEqualTo(LocalDate.of(2020, 1, 31));
+
+  @Nested
+  class YearMonthから月末を求める {
+
+
+    @Test
+    void test_01() {
+      assertThat(
+          YearMonth.parse("2020-01").atEndOfMonth()
+      ).isEqualTo(LocalDate.of(2020, 1, 31));
+    }
+
+    @Test
+    void test_02() {
+      assertThat(
+          YearMonth.parse("2020-04").atEndOfMonth()
+      ).isEqualTo(LocalDate.of(2020, 4, 30));
+    }
+
+    @Test
+    void test_03() {
+      assertThat(
+          YearMonth.parse("2020-02").atEndOfMonth()
+      ).isEqualTo(LocalDate.of(2020, 2, 29));
+    }
+
+    @Test
+    void test_04() {
+      assertThat(
+          YearMonth.parse("2021-02").atEndOfMonth()
+      ).isEqualTo(LocalDate.of(2021, 2, 28));
+    }
   }
 
-  @Test
-  void test_02() {
-    assertThat(
-        YearMonth.parse("2020-04").atEndOfMonth()
-    ).isEqualTo(LocalDate.of(2020, 4, 30));
-  }
+  @Nested
+  class LocalDateから月末 {
 
-  @Test
-  void test_03() {
-    assertThat(
-        YearMonth.parse("2020-02").atEndOfMonth()
-    ).isEqualTo(LocalDate.of(2020, 2, 29));
-  }
 
-  @Test
-  void test_04() {
-    assertThat(
-        YearMonth.parse("2021-02").atEndOfMonth()
-    ).isEqualTo(LocalDate.of(2021, 2, 28));
+    @Test
+    void test_01() {
+      assertThat(
+          YearMonth.from(
+              LocalDate.of(2020, 1, 15)
+          ).atEndOfMonth()
+      ).isEqualTo(LocalDate.of(2020, 1, 31));
+    }
+
+    @Test
+    void test_02() {
+      assertThat(
+          YearMonth.from(
+              LocalDate.of(2020,4, 15)
+          ).atEndOfMonth()
+      ).isEqualTo(LocalDate.of(2020, 4, 30));
+    }
+
+    @Test
+    void test_03() {
+      assertThat(
+          YearMonth.from(
+              LocalDate.of(2020,2, 15)
+          ).atEndOfMonth()
+      ).isEqualTo(LocalDate.of(2020, 2, 29));
+    }
+
+    @Test
+    void test_04() {
+      assertThat(
+          YearMonth.from(
+              LocalDate.of(2021,2, 15)
+          ).atEndOfMonth()
+      ).isEqualTo(LocalDate.of(2021, 2, 28));
+    }
+
   }
 }
