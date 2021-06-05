f0d1f7fdbbc2f38a8d40d7bb2293f5326e2b8003
nainaistar <nainaistar@gmail.com>
2021-06-05 20:58:44 +0900

    [Add] 17歳計算ロジック

--- src/main/java/kirimaru/biz/service/IAm17Impl.java
+++ src/main/java/kirimaru/biz/service/IAm17Impl.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,8 +18,18 @@ import java.util.List;
 public class IAm17Impl {
   private final SystemDateTimeResolver systemDateTimeResolver;
 
-  public String iam17(LocalDateTime birth) {
-    return "";
+  public String iam17(LocalDate birth) {
+    LocalDateTime now = systemDateTimeResolver.now();
+    int year = now.getYear() - birth.getYear();
+    int month = now.getMonthValue() - birth.getMonthValue();
+    if (birth.getYear() >= 17) {
+      month = month + ((year - 17) * 12);
+      year = 17;
+    } else {
+      year = birth.getYear();
+    }
+
+    return year + "歳" + month + "ヵ月";
   }
 
 
--- src/test/java/kirimaru/biz/service/IAm17ImplTests.java
+++ src/test/java/kirimaru/biz/service/IAm17ImplTests.java
@@ -7,10 +7,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class IAm17ImplTests {
@@ -21,8 +23,17 @@ class IAm17ImplTests {
 
   @Test
   void test_01() {
-    assertThat(target.iam17(LocalDateTime.now()))
-        .isEqualTo("");
-    ;
+    when(systemDateTimeResolver.now()).thenReturn(LocalDateTime.of(2021, 6, 5, 0, 0));
+
+    assertThat(target.iam17(LocalDate.of(1992, 2, 4)))
+        .isEqualTo("17歳148ヵ月");
+  }
+
+  @Test
+  void test_02() {
+    when(systemDateTimeResolver.now()).thenReturn(LocalDateTime.of(2021, 6, 5, 0, 0));
+
+    assertThat(target.iam17(LocalDate.of(1992, 3, 4)))
+        .isEqualTo("17歳147ヵ月");
   }
 }
