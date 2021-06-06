af6e87fe6d3dabfd24b199479c5a8906e0fd4797
nainaistar <nainaistar@gmail.com>
2021-06-07 00:18:48 +0900

    [Add] 追加

--- src/main/java/kirimaru/config/TwitterProperties.java
+++ src/main/java/kirimaru/config/TwitterProperties.java
@@ -2,10 +2,14 @@ package kirimaru.config;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Getter
 @Setter
+@Configuration
 @ConfigurationProperties(prefix = "external.twitter")
+@ToString(callSuper = true)
 public class TwitterProperties extends ExternalProperties {
 }
