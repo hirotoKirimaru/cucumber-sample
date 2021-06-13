a464921bf0d44793732ff63c2111c5bac29cba3c
nainaistar <nainaistar@gmail.com>
2021-06-13 21:21:42 +0900

    [Add] Import

--- kirimaru-core/src/main/java/kirimaru/biz/domain/date/DateTimeResolver.java
+++ kirimaru-core/src/main/java/kirimaru/biz/domain/date/DateTimeResolver.java
@@ -1,10 +1,4 @@
 package kirimaru.biz.domain.date;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
 public interface DateTimeResolver {
 }
--- kirimaru-repository/src/test/java/kirimaru/biz/mapper/BookMapperTests.java
+++ kirimaru-repository/src/test/java/kirimaru/biz/mapper/BookMapperTests.java
@@ -1,20 +1,13 @@
 package kirimaru.biz.mapper;
 
-import kirimaru.biz.domain.book.Book;
-import kirimaru.biz.domain.book.Isbn;
-import kirimaru.biz.domain.date.SystemDateTimeResolver;
 import kirimaru.biz.mapper.dto.BookDto;
 import kirimaru.biz.mapper.helper.CommonSetup;
-import kirimaru.helper.ApplicationContextAccessorConfig;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static reactor.core.publisher.Mono.when;
 
 public class BookMapperTests extends CommonSetup {
   @Autowired
