af7ba9eb6632319c69845587cd328cbdb5b719fa
nainaistar <nainaistar@gmail.com>
2021-06-13 20:50:58 +0900

    [Add] MapperScanを削除

--- kirimaru-repository/src/main/java/kirimaru/biz/mapper/MapperScanConfig.java
+++ kirimaru-repository/src/main/java/kirimaru/biz/mapper/MapperScanConfig.java
@@ -3,9 +3,9 @@ package kirimaru.biz.mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration("MapperScanConfig")
-@MapperScan(basePackages = {
-    "kirimaru.biz"
-})
-public class MapperScanConfig {
-}
+//@Configuration("MapperScanConfig")
+//@MapperScan(basePackages = {
+//    "kirimaru.biz"
+//})
+//public class MapperScanConfig {
+//}
