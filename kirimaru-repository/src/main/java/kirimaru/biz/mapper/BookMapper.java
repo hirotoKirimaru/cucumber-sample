993d839565546b981e92a798f1dd61955791cd1f
nainaistar <nainaistar@gmail.com>
2021-06-13 17:24:16 +0900

    [Add] InsertProviderを使うようにする

--- kirimaru-repository/src/main/java/kirimaru/biz/mapper/BookMapper.java
+++ kirimaru-repository/src/main/java/kirimaru/biz/mapper/BookMapper.java
@@ -2,6 +2,7 @@ package kirimaru.biz.mapper;
 
 import kirimaru.biz.mapper.dto.BookDto;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.InsertProvider;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
@@ -17,11 +18,16 @@ public interface BookMapper {
       """})
   List<BookDto> findAll();
 
-
-//  INSERT INTO BOOK VALUES('9784798126708', '1000', 'kirimaru', '2021/06/13', 'kirimaru', '2021/06/13', 'kirimaru')
-  @Insert("""
-          INSERT INTO BOOK VALUES(#{isbn}, #{money}, #{author}, #{generateDate}, #{generateUser}, #{updateDate}, #{updateUser})
-      """
-  )
+  @InsertProvider(type = BookMapper.ScriptBuilder.class, method = "insert")
   int insert(BookDto book);
+
+  public static class ScriptBuilder {
+    public String insert() {
+      return new StringBuilder()
+          .append(
+              "INSERT INTO BOOK VALUES(#{isbn}, #{money}, #{author}, #{generateDate}, #{generateUser}, #{updateDate}, #{updateUser})"
+          ).toString();
+    }
+  }
 }
+
