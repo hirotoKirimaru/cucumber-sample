6ae223bb6ef5ed83fe2d4f7a48a0456fc217fdac
nainaistar <nainaistar@gmail.com>
2021-06-17 00:11:08 +0900

    [Add] Concatやる

---
+++ kirimaru-core/src/main/java/kirimaru/biz/domain/Concat.java
@@ -0,0 +1,83 @@
+package kirimaru.biz.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.text.MessageFormat;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.StringJoiner;
+
+
+public class Concat {
+
+
+  public String joiner(List<String> words) {
+
+    var sj = new StringJoiner(",");
+    for (String word : words) {
+      sj.add(word);
+    }
+    return sj.toString();
+  }
+
+  public String builder(List<String> words) {
+    var sb = new StringBuilder();
+    for (String word : words) {
+      sb.append(word);
+      sb.append(",");
+    }
+    sb.delete(sb.length() - 1, sb.length());
+
+    return sb.toString();
+  }
+
+  public String builder2(List<String> words) {
+    var sb = new StringBuilder();
+    for (int i = 0; i < words.size(); i++) {
+      sb.append(words.get(i));
+      if (words.size() - 1 != i) {
+        sb.append(",");
+      }
+    }
+
+    return sb.toString();
+  }
+
+  static class StringJoinerPractice {
+
+    public String tab(List<String> words) {
+      var sj = new StringJoiner("\t");
+      for (String word : words) {
+        sj.add(word);
+      }
+      return sj.toString();
+    }
+
+    public String addAiueo(List<String> words) {
+      var sj = new StringJoiner("AIUEO");
+      for (String word : words) {
+        sj.add(word);
+      }
+      return sj.toString();
+    }
+
+    public String addBracket(List<String> words) {
+      var sj = new StringJoiner(",", "{", "}");
+      for (String word : words) {
+        sj.add(word);
+      }
+      return sj.toString();
+    }
+
+    public String addBracketAndDoubleQuote(List<String> words) {
+      var sj = new StringJoiner(",", "{", "}");
+      for (String word : words) {
+        sj.add(MessageFormat.format("\"{0}\"", word));
+      }
+      return sj.toString();
+    }
+  }
+
+}
---
+++ kirimaru-core/src/test/java/kirimaru/biz/domain/ConcatTests.java
@@ -0,0 +1,68 @@
+package kirimaru.biz.domain;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ConcatTests {
+  Concat target = new Concat();
+  List<String> param = List.of("りんご", "ゴリラ", "ラッパ");
+  String expect = "りんご,ゴリラ,ラッパ";
+
+  @Test
+  void test_01() {
+    assertThat(
+        target.joiner(param)
+    ).isEqualTo(expect);
+  }
+
+  @Test
+  void test_02() {
+    assertThat(
+        target.builder(param)
+    ).isEqualTo(expect);
+  }
+
+  @Test
+  void test_03() {
+    assertThat(
+        target.builder2(param)
+    ).isEqualTo(expect);
+  }
+
+  @Nested
+  class Joiner {
+    Concat.StringJoinerPractice target = new Concat.StringJoinerPractice();
+
+    @Test
+    void test_01() {
+      assertThat(
+          target.tab(param)
+      ).isEqualTo("りんご\tゴリラ\tラッパ");
+    }
+
+    @Test
+    void test_02() {
+      assertThat(
+          target.addAiueo(param)
+      ).isEqualTo("りんごAIUEOゴリラAIUEOラッパ");
+    }
+
+    @Test
+    void test_03() {
+      assertThat(
+          target.addBracket(param)
+      ).isEqualTo("{りんご,ゴリラ,ラッパ}");
+    }
+
+    @Test
+    void test_04() {
+      assertThat(
+          target.addBracketAndDoubleQuote(param)
+      ).isEqualTo("{\"りんご\",\"ゴリラ\",\"ラッパ\"}");
+    }
+  }
+}
