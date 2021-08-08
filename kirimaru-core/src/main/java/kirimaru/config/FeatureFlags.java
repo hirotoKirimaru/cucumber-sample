ae2dc9ec85c8c5e4bb17082b1f063d9648a256c1
水上 皓登 <nainaistar@gmail.com>
2021-08-08 15:55:00 +0900

    [Add] こんなイメージ？

---
+++ kirimaru-core/src/main/java/kirimaru/config/FeatureFlags.java
@@ -0,0 +1,24 @@
+package kirimaru.config;
+
+import no.finn.unleash.DefaultUnleash;
+import no.finn.unleash.Unleash;
+import no.finn.unleash.UnleashContextProvider;
+import no.finn.unleash.util.UnleashConfig;
+import org.springframework.context.annotation.Bean;
+
+public class FeatureFlags {
+
+  @Bean
+  public Unleash unleash() {
+//    UnleashContextProvider contextProvider = new MyAwesomeContextProvider();
+    UnleashConfig config = new UnleashConfig.Builder()
+        .appName("java-test")
+        .instanceId("instance x")
+        .unleashAPI("http://unleash.herokuapp.com/api/")
+//        .unleashContextProvider(contextProvider)
+        .build();
+
+    Unleash unleash = new DefaultUnleash(config);
+    return unleash;
+  }
+}
