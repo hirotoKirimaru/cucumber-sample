41d59245ddc30f758a39bfb69d2096ee8e425e11
水上 皓登 <nainaistar@gmail.com>
2021-08-09 15:10:35 +0900

    [Add] versionをargsから渡す

---
+++ kirimaru-core/src/main/java/kirimaru/config/VersionConfig.java
@@ -0,0 +1,17 @@
+package kirimaru.config;
+
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class VersionConfig {
+    @Value("${version}")
+    String version;
+
+    @PostConstruct
+    public void after(){
+        System.out.println("***************");
+        System.out.println(version);
+    }
+}
