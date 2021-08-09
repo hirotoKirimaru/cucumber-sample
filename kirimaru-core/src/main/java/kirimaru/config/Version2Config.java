b8f5b84778d58f542068046d6581c07df2bc0ed7
水上 皓登 <nainaistar@gmail.com>
2021-08-09 21:12:11 +0900

    やっぱりコメント

--- kirimaru-core/src/main/java/kirimaru/config/Version2Config.java
+++ kirimaru-core/src/main/java/kirimaru/config/Version2Config.java
@@ -7,18 +7,19 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Getter
-@Setter
-@Configuration
-@ConfigurationProperties(prefix = "info")
-class Version2Config {
-
-  Version version;
-
-  @PostConstruct
-  public void after() {
-    System.out.println("Version2");
-    System.out.println("***************");
-    System.out.println(version);
-  }
-}
+// エラーになる？
+//@Getter
+//@Setter
+//@Configuration
+//@ConfigurationProperties(prefix = "info")
+//class Version2Config {
+//
+//  Version version;
+//
+//  @PostConstruct
+//  public void after() {
+//    System.out.println("Version2");
+//    System.out.println("***************");
+//    System.out.println(version);
+//  }
+//}
