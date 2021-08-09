2a96d0f629eb006f44803c60c178d965d85c02dd
水上 皓登 <nainaistar@gmail.com>
2021-08-09 21:16:38 +0900

    もっとコメント

--- kirimaru-core/src/main/java/kirimaru/config/VersionConfig.java
+++ kirimaru-core/src/main/java/kirimaru/config/VersionConfig.java
@@ -4,14 +4,14 @@ import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-class VersionConfig {
-    @Value("${version}")
-    Version version;
-
-    @PostConstruct
-    public void after(){
-        System.out.println("***************");
-        System.out.println(version);
-    }
-}
+//@Configuration
+//class VersionConfig {
+//    @Value("${version}")
+//    Version version;
+//
+//    @PostConstruct
+//    public void after(){
+//        System.out.println("***************");
+//        System.out.println(version);
+//    }
+//}
