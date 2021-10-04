379f74fa5c5a910ad0b2fe03e6e7e9c5499f00aa
水上 皓登 <nainaistar@gmail.com>
2021-10-04 23:50:16 +0900

    実DBを使ってテストをする方法

--- kirimaru-repository/src/test/java/kirimaru/biz/mapper/helper/CommonSetup.java
+++ kirimaru-repository/src/test/java/kirimaru/biz/mapper/helper/CommonSetup.java
@@ -1,26 +1,27 @@
 package kirimaru.biz.mapper.helper;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import kirimaru.biz.domain.book.Isbn;
-import kirimaru.biz.domain.constant.CodeConstant;
-import kirimaru.biz.domain.book.Book;
-import kirimaru.biz.mapper.dto.Book2Dto;
-import kirimaru.biz.mapper.dto.BookDto;
-import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-
-import javax.sql.DataSource;
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.sql.DataSource;
+import kirimaru.biz.domain.book.Book;
+import kirimaru.biz.domain.book.Isbn;
+import kirimaru.biz.domain.constant.CodeConstant;
+import kirimaru.biz.mapper.dto.Book2Dto;
+import kirimaru.biz.mapper.dto.BookDto;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 @MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CommonSetup {
   @Autowired
   JdbcTemplate jdbcTemplate;
