c21b32d80913f0393b5cf149e4f57dcea7b19274
水上 皓登 <nainaistar@gmail.com>
2022-01-03 11:03:49 +0900

    refactor: rm

--- kirimaru-backend/src/test/java/kirimaru/config/CorePropertiesTests.java
+++ kirimaru-backend/src/test/java/kirimaru/config/CorePropertiesTests.java
@@ -1,13 +1,12 @@
 package kirimaru.config;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringJUnitConfig(initializers = ConfigDataApplicationContextInitializer.class)
 @EnableConfigurationProperties({CoreProperties.class})
--- kirimaru-backend/src/test/java/kirimaru/config/FacebookPropertiesTests.java
+++ kirimaru-backend/src/test/java/kirimaru/config/FacebookPropertiesTests.java
@@ -1,16 +1,13 @@
 package kirimaru.config;
 
-import org.junit.jupiter.api.Nested;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.URI;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-
-import java.net.URI;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringJUnitConfig(initializers = ConfigDataApplicationContextInitializer.class)
 @EnableConfigurationProperties({FacebookProperties.class})
