774660e3dca3fe897cf611c7e0e06acda0409a67
水上 皓登 <nainaistar@gmail.com>
2022-03-26 15:32:48 +0900

    docs: yieldとreturnの比較等々

--- kirimaru-core/src/test/java/kirimaru/biz/domain/SwitchExpressionTests.java
+++ kirimaru-core/src/test/java/kirimaru/biz/domain/SwitchExpressionTests.java
@@ -1,5 +1,8 @@
 package kirimaru.biz.domain;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
 /**
  * https://docs.oracle.com/en/java/javase/17/language/switch-expressions.html
  */
@@ -15,9 +18,8 @@ class SwitchExpressionTests {
     SATURDAY;
   }
 
-  public int returnValue() {
+  public int returnValue(Day day) {
     int numLetters = 0;
-    Day day = Day.WEDNESDAY;
     switch (day) {
       case MONDAY:
       case FRIDAY:
@@ -41,8 +43,7 @@ class SwitchExpressionTests {
     return numLetters;
   }
 
-  public int returnValu2() {
-    Day day = Day.WEDNESDAY;
+  public int returnValue2(Day day) {
     int numLetters = switch (day) {
       case MONDAY, FRIDAY, SUNDAY -> 6;
       case TUESDAY -> 7;
@@ -53,4 +54,107 @@ class SwitchExpressionTests {
     System.out.println(numLetters);
     return numLetters;
   }
+
+  public int returnValue3(Day day) {
+    int numLetters = switch (day) {
+      case MONDAY:
+      case FRIDAY:
+      case SUNDAY:
+        System.out.println(6);
+        yield 6;
+      case TUESDAY:
+        System.out.println(7);
+        yield 7;
+      case THURSDAY:
+      case SATURDAY:
+        System.out.println(8);
+        yield 8;
+      case WEDNESDAY:
+        System.out.println(9);
+        yield 9;
+      default:
+        throw new IllegalStateException("Invalid day: " + day);
+    };
+    return numLetters;
+  }
+
+  public int returnValue4(Day day) {
+    int numLetters = switch (day) {
+      case MONDAY, FRIDAY, SUNDAY -> {
+        System.out.println(6);
+        yield 6;
+      }
+      case TUESDAY -> {
+        System.out.println(7);
+        yield 7;
+      }
+      case THURSDAY, SATURDAY -> {
+        System.out.println(8);
+        yield 8;
+      }
+      case WEDNESDAY -> {
+        System.out.println(9);
+        yield 9;
+      }
+      default -> {
+        throw new IllegalStateException("Invalid day: " + day);
+      }
+    };
+    return numLetters;
+  }
+
+  public int returnValue5(Day day) {
+    switch (day) {
+      case MONDAY, FRIDAY, SUNDAY -> {
+        System.out.println(6);
+        return 6;
+      }
+      case TUESDAY -> {
+        System.out.println(7);
+        return 7;
+      }
+      case THURSDAY, SATURDAY -> {
+        System.out.println(8);
+        return 8;
+      }
+      case WEDNESDAY -> {
+        System.out.println(9);
+        return 9;
+      }
+      default -> {
+        throw new IllegalStateException("Invalid day: " + day);
+      }
+    }
+  }
+
+  @EnumSource(value = Day.class)
+  @ParameterizedTest
+  public void test_01(Day day) {
+    System.out.println(returnValue(day));
+  }
+
+  @EnumSource(value = Day.class)
+  @ParameterizedTest
+  public void test_02(Day day) {
+    System.out.println(returnValue2(day));
+  }
+
+  @EnumSource(value = Day.class)
+  @ParameterizedTest
+  public void test_03(Day day) {
+    System.out.println(returnValue3(day));
+  }
+
+  @EnumSource(value = Day.class)
+  @ParameterizedTest
+  public void test_04(Day day) {
+    System.out.println(returnValue4(day));
+  }
+
+  @EnumSource(value = Day.class)
+  @ParameterizedTest
+  public void test_05(Day day) {
+    System.out.println(returnValue5(day));
+  }
+
 }
