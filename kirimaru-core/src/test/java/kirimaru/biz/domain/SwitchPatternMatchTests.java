85fe4ae266cb403c85755e48076d7a176d8382ce
水上 皓登 <nainaistar@gmail.com>
2022-03-26 15:16:55 +0900

    feat: instance of で変数に渡せる

--- kirimaru-core/src/test/java/kirimaru/biz/domain/SwitchPatternMatchTests.java
+++ kirimaru-core/src/test/java/kirimaru/biz/domain/SwitchPatternMatchTests.java
@@ -27,6 +27,16 @@ class SwitchPatternMatchTests {
       }
     }
 
+    public static double getPerimeter2(Shape shape) throws IllegalArgumentException {
+      if (shape instanceof Rectangle r) {
+        return 2 * r.length() + 2 * r.width();
+      } else if (shape instanceof Circle c) {
+        return 2 * c.radius() * Math.PI;
+      } else {
+        throw new IllegalArgumentException("Unrecognized shape");
+      }
+    }
+
 //    public static double getPerimeter2(Shape shape) throws IllegalArgumentException {
 //      return switch (shape) {
 //        case Rectangle r -> 2 * r.length() + 2 * r.width();
