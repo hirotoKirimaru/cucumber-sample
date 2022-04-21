8e8915d4d41f777a2f3967bcba0dba00ddd1dcf8
水上 皓登 <nainaistar@gmail.com>
2022-04-21 23:40:39 +0900

    feat: 複数groupingByをする。処理の目的はあんまりない

--- kirimaru-core/src/test/java/kirimaru/biz/domain/MultiGroupingTests.java
+++ kirimaru-core/src/test/java/kirimaru/biz/domain/MultiGroupingTests.java
@@ -2,7 +2,9 @@ package kirimaru.biz.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.junit.jupiter.api.Disabled;
@@ -19,7 +21,7 @@ class MultiGroupingTests {
 
   }
 
-  @Disabled("なにもしていない")
+//  @Disabled("なにもしていない")
   @Test
   void test_01() {
     var param = List.of(
@@ -28,15 +30,33 @@ class MultiGroupingTests {
         new Book("3", "Japanese", 1990, "JP", new Author("しんべえ")),
         new Book("4", "Japanese", 1990, "JP", new Author("きり丸"))
     );
-//    var actual = param.stream().collect(
-//        Collectors.groupingBy(Book::language,
-//            Collectors.groupingBy(Book::year, Collectors.groupingBy(Book::salesTerritory))
-//        ));
+    var actual = groupingList(param);
 
-//    var expected = actual;
+    var expected = List.of(
+        new Book("1", "Japanese", 1990, "JP", new Author("きり丸")),
+        new Book("4", "Japanese", 1990, "JP", new Author("きり丸")),
+        new Book("2", "Japanese", 1990, "JP", new Author("乱太郎")),
+        new Book("3", "Japanese", 1990, "JP", new Author("しんべえ"))
+    );
 
-    assertThat(param).containsAll(param);
+    assertThat(actual).isEqualTo(expected);
+  }
 
+  private List<Book> groupingList(List<Book> param) {
+    var group = param.stream().collect(
+        Collectors.groupingBy(Book::language,
+            Collectors.groupingBy(Book::year,
+                Collectors.groupingBy(Book::author))
+        ));
 
+    List list = new ArrayList();
+    for (Map<Integer, Map<Author, List<Book>>> value : group.values()) {
+      for (Map<Author, List<Book>> stringListMap : value.values()) {
+        for (List<Book> books : stringListMap.values()) {
+          list.addAll(books);
+        }
+      }
+    }
+    return list;
   }
 }
