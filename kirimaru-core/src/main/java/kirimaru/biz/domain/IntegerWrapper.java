aa4b5ad0ef1ab5fb6170b5fce93a55786d74ca11
水上 皓登 <nainaistar@gmail.com>
2022-06-25 23:03:15 +0900

    feat: 個のバグなかった

---
+++ kirimaru-core/src/main/java/kirimaru/biz/domain/IntegerWrapper.java
@@ -0,0 +1,15 @@
+package kirimaru.biz.domain;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+@Data
+public class IntegerWrapper {
+    private Integer integer;
+}
---
+++ kirimaru-core/src/test/java/kirimaru/biz/domain/IntegerWrapperTests.java
@@ -0,0 +1,30 @@
+package kirimaru.biz.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class IntegerWrapperTests {
+  @Nested
+  class ToBuilder {
+    IntegerWrapper integerWrapper = new IntegerWrapper();
+    @Test
+    void test_01() {
+      IntegerWrapper target = IntegerWrapper.builder().build();
+
+      assertThat(target).isEqualTo(integerWrapper);
+    }
+
+    @Test
+    void test_02() {
+      // バージョンによってはなぜかこれで0が設定されるバグがあった
+      IntegerWrapper target = IntegerWrapper.builder().build();
+      target = target.toBuilder().build();
+
+      assertThat(target).isEqualTo(integerWrapper);
+    }
+  }
+
+}
