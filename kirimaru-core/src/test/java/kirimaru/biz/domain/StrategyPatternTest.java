ccf48b2c8deb1bf028ad6a245aaca1605981ca78
水上 皓登 <nainaistar@gmail.com>
2022-07-18 00:19:14 +0900

    feat: genreでGroupingする処理を追加

--- kirimaru-core/src/test/java/kirimaru/biz/domain/StrategyPatternTest.java
+++ kirimaru-core/src/test/java/kirimaru/biz/domain/StrategyPatternTest.java
@@ -4,20 +4,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class StrategyPatternTest {
 
-  @Test
-  void test_01() {
-    var target = new QuestionList(
-        List.of(
-            Question.builder().build()
-        )
-    );
+  @Nested
+  class groupByGenre {
 
-    assertThat(
-        target.groupByGenre()
-    ).isEqualTo(Map.of(0, List.of(Question.builder().build())));
+    @Test
+    void test_01() {
+      var target = new QuestionList(
+          List.of(
+              Question.builder().build()
+          )
+      );
+
+      assertThat(
+          target.groupByGenre()
+      ).isEqualTo(Map.of(0, List.of(Question.builder().build())));
+    }
+
+    @Test
+    void test_02() {
+      var target = new QuestionList(
+          List.of(
+              Question.builder().genre(0).build(),
+              Question.builder().genre(1).build(),
+              Question.builder().genre(1).build(),
+              Question.builder().genre(2).build(),
+              Question.builder().genre(4).build()
+          )
+      );
+
+      assertThat(
+          target.groupByGenre()
+      ).containsAllEntriesOf(
+          Map.of(
+              0, List.of(Question.builder().genre(0).build()),
+              1, List.of(Question.builder().genre(1).build(), Question.builder().genre(1).build()),
+              2, List.of(Question.builder().genre(2).build()),
+              4, List.of(Question.builder().genre(4).build())
+          )
+      );
+    }
   }
+
 }
