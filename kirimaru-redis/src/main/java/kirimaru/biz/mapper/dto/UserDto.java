3397588b67623d9fd1e5e10076c647ca8787277b
水上 皓登 <nainaistar@gmail.com>
2022-11-06 19:56:13 +0900

    feat: Redisのテスト

--- kirimaru-redis/src/main/java/kirimaru/biz/mapper/dto/UserDto.java
+++ kirimaru-redis/src/main/java/kirimaru/biz/mapper/dto/UserDto.java
@@ -2,12 +2,14 @@ package kirimaru.biz.mapper.dto;
 
 import java.io.Serializable;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
 @RedisHash("user")
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto implements Serializable {
--- kirimaru-redis/src/test/java/kirimaru/biz/mapper/UserRedisMapperTests.java
+++ kirimaru-redis/src/test/java/kirimaru/biz/mapper/UserRedisMapperTests.java
@@ -3,6 +3,8 @@ package kirimaru.biz.mapper;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+import kirimaru.biz.mapper.dto.UserDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
@@ -18,5 +20,17 @@ class UserRedisMapperTests {
     assertThat(
         mapper.findAll()
     ).isEmpty();
+  }
+
+  @Test
+  void test_02() {
+    var USER1 = UserDto.builder().id("1").name("1").build();
+    var USER2 = UserDto.builder().id("2").name("2").build();
+
+    mapper.saveAll(List.of(USER1, USER2));
+
+    assertThat(
+        mapper.findAll()
+    ).isEqualTo(List.of(USER1, USER2));
   }
 }
