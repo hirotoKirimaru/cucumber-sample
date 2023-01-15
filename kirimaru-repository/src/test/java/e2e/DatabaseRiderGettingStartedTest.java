7c885df6ee7777c015f954805b42ef5cb50e2a41
水上 皓登 <nainaistar@gmail.com>
2023-01-15 14:08:00 +0900

    WIP

---
+++ kirimaru-repository/src/test/java/e2e/DatabaseRiderGettingStartedTest.java
@@ -0,0 +1,9 @@
+package e2e;
+
+import com.github.database.rider.junit5.DBUnitExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(DBUnitExtension.class)
+public class DatabaseRiderGettingStartedTest {
+
+}
