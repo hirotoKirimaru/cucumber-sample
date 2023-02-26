2626f94a4758e1f3e2bd8ced7df21d2e443dbb7a
水上 皓登 <nainaistar@gmail.com>
2023-02-27 00:33:54 +0900

    [feat] 0右埋め

--- kirimaru-core/src/test/java/kirimaru/biz/domain/AppStringTest.java
+++ kirimaru-core/src/test/java/kirimaru/biz/domain/AppStringTest.java
@@ -1,8 +1,11 @@
 package kirimaru.biz.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.IllegalFormatConversionException;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -141,11 +144,39 @@ class AppStringTest {
     }
 
     @Test
+    void test_02_02() {
+      // formatが合わないとエラーがでる
+      assertThatThrownBy(
+          () -> "%03d".formatted("1")
+      ).isInstanceOf(IllegalFormatConversionException.class);
+    }
+
+    @Test
     void test_03() {
       // もーちょっとちゃんとしたやつないのかな・・・
       assertThat(
-          String.format("%3s", 1).replace(" ", "X")
+          "%3s".formatted(1).replace(" ", "X")
       ).isEqualTo("XX1");
+    }
+  }
+
+  @Nested
+  class PadRight {
+
+    @Test
+    @Disabled("ずれるし、むりか")
+    void test_01() {
+      // 右0埋めを数字ですることは無理みたい
+      assertThat(
+          "%0-3d".formatted("1")
+      ).isEqualTo("100");
+    }
+
+    @Test
+    void test_02() {
+      assertThat(
+          "%-3s".formatted(1).replace(" ", "X")
+      ).isEqualTo("1XX");
     }
   }
 }
